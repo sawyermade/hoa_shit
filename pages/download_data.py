c6cc7ca2f24cd9b9b312ad5ffa3f26cd09ed084d
SawyerMade <sawyermade0@gmail.com>
2025-01-29 19:06:38 -0500

    code cleanup

--- pages/download_data.py
+++ pages/download_data.py
@@ -9,7 +9,7 @@ st.title('Download Data')
 # Load links for csv files
 data_output_dir = 'data/output/'
 if os.path.exists(data_output_dir):
-    files = sorted([f for f in os.listdir('data/output/') if f.endswith('.csv')])
+    files = sorted([f for f in os.listdir(data_output_dir) if f.endswith('.csv')])
     fpaths = [os.path.join(data_output_dir, f) for f in files]
     for f, fp in zip(files, fpaths):
         with open(fp, 'rb') as file:
